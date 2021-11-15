4b5c53d616ac088dc43b360c085b0bd6077a4d1a
Pajri Romdhoni Isyana <78802144+pppjrre@users.noreply.github.com>
2021-11-15 11:02:39 +0700

    pbo_pajri
    Tugas aplikasi UTS Pajri Romdhoni Isyana
    XII RPL 1

---
+++ pbo_pajri/Views/Home.php
@@ -0,0 +1,14 @@
+<body>
+
+<div class="p-3 mb-2 bg-info font-weight-bold text-danger">Project PTS PBO</div>
+  
+  <div class="card-body bg-primary">
+    <p class="card-text font-weight-bold text-danger bg-info">
+        Pajri Romdhoni Isyana <br>
+        XII RPL-1 <br>
+        Absen 22
+    </p>
+  </div>
+</div>
+
+</body>
